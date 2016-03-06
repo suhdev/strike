959a9fafa414da6cfc4791fe297e4216e9d1a36b
Suhail Abood <suhail.abood@gmail.com>
2016-03-06 15:32:00 +0000

    fixes

--- index.js
+++ index.js
@@ -3,11 +3,6 @@ import {Store} from './lib/core/Store';
 import {Promisify} from './lib/middleware/PromisifyMiddleware';
 import {ControllerView} from './lib/react/ControllerView';
 
-export const Combiner = Combiner;
-export const Store = Store;
-export const Promisify = Promisify;
-export const ControllerView = ControllerView; 
-
 export default {
 	Combiner,
 	Store,
