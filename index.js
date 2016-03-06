e4e3afc30ea6e249669ab3e5001da8e21ac8c2af
Suhail Abood <suhail.abood@gmail.com>
2016-03-06 21:35:14 +0000

    fixes

--- index.js
+++ index.js
@@ -2,10 +2,21 @@
 const Combiner = require('./lib/core/Combiner').Combiner;
 const Store = require('./lib/core/Store').Store;
 const Promisify = require('./lib/middleware/PromisifyMiddleware').Promisify;
-const ControllerView = require('./lib/react/ControllerView').ControllerView; 
+const ControllerView = require('./lib/react/ControllerView').ControllerView;
+const Injector = require('./lib/core/Injector').Injector;
+const Util = require('./lib/core/Util');
+const extractArgumentsFromFunction = Util.extractArgumentsFromFunction;
+const getDataAt = Util.getDataAt;
+const setDataAt = Util.setDataAt;
+const format = Util.format;  
+
 module.exports = {
 	Combiner:Combiner,
 	Store:Store,
 	Promisify:Promisify,
-	ControllerView:ControllerView
+	ControllerView:ControllerView,
+	extractArgumentsFromFunction:extractArgumentsFromFunction,
+	getDataAt:getDataAt,
+	setDataAt:setDataAt,
+	format:format
 };
