208d143f7b260969020dda8f76af109eba8bb122
Suhail Abood <suhail.abood@gmail.com>
2016-03-06 00:04:03 +0000

    adds index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+import {Combiner} from './lib/core/Combiner'; 
+import {Store} from './lib/core/Store'; 
+import {Promisify} from './lib/middleware/PromisifyMiddleware';
+import {ControllerView} from './lib/react/ControllerView';
+
+export Combiner;
+export Store;
+export Promisify;
+export ControllerView; 
+
+export default {
+	Combiner,
+	Store,
+	Promisify,
+	ControllerView
+};
