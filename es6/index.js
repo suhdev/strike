b0f15931714ab03ae81105ab58ee41095e748fef
Suhail Abood <suhail.abood@gmail.com>
2016-05-21 11:02:41 +0100

    adds PROMISIFY constants

--- es6/index.js
+++ es6/index.js
@@ -5,9 +5,9 @@ import {extractArgumentsFromFunction,getDataAt,setDataAt,format} from './core/Ut
 import {Injectable} from './middleware/InjectableMiddleware'; 
 import {Promisify} from './middleware/PromisifyMiddleware';
 import {WorkerMiddleware} from './middleware/WorkerMiddleware';
-import {IntegerPromisify} from './middleware/IntegerPromisifyMiddleware';
+import {IntegerPromisify,PROMISE_FETCHING,PROMISE_RESOLVED,PROMISE_REJECTED,PROMISE_NOTIFY} from './middleware/IntegerPromisifyMiddleware';
 import {ControllerView} from './react/ControllerView';
 
 export {Combiner,Store,Injector,extractArgumentsFromFunction,getDataAt,setDataAt,format,
-	Promisify,ControllerView,IntegerPromisify,
+	Promisify,ControllerView,IntegerPromisify,PROMISE_NOTIFY,PROMISE_REJECTED,PROMISE_RESOLVED,PROMISE_FETCHING,
 	Injectable}
--- index.js
+++ index.js
@@ -2,7 +2,12 @@
 const Combiner = require('./lib/core/Combiner').Combiner;
 const Store = require('./lib/core/Store').Store;
 const Promisify = require('./lib/middleware/PromisifyMiddleware').Promisify;
-const IntegerPromisify = require('./lib/middleware/IntegerPromisifyMiddleware').IntegerPromisify;
+const IntegerPromisifyMiddleware = require('./lib/middleware/IntegerPromisifyMiddleware');
+const IntegerPromisify = IntegerPromisifyMiddleware.IntegerPromisify;
+const PROMISE_FETCHING = IntegerPromisifyMiddleware.PROMISE_FETCHING; 
+const PROMISE_RESOLVED = IntegerPromisifyMiddleware.PROMISE_RESOLVED; 
+const PROMISE_REJECTED = IntegerPromisifyMiddleware.PROMISE_REJECTED; 
+const PROMISE_NOTIFY = IntegerPromisifyMiddleware.PROMISE_NOTIFY; 
 const Injectable = require('./lib/middleware/InjectableMiddleware').Injectable;
 const WorkerMiddleware = require('./lib/middleware/WorkerMiddleware').WorkerMiddleware;
 const ControllerView = require('./lib/react/ControllerView').ControllerView;
@@ -19,6 +24,10 @@ module.exports = {
 	Promisify:Promisify,
 	Injectable:Injectable,
 	Injector:Injector,
+	PROMISE_NOTIFY:PROMISE_NOTIFY,
+	PROMISE_RESOLVED:PROMISE_RESOLVED,
+	PROMISE_REJECTED:PROMISE_REJECTED,
+	PROMISE_FETCHING:PROMISE_FETCHING,
 	IntegerPromisify:IntegerPromisify,
 	WorkerMiddleware:WorkerMiddleware,
 	ControllerView:ControllerView,
